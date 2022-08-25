cdbf221103b591387425f19a8c2507a4f46b503e
Nirlipta Pande <nirlipta.pande@geo.tuwien.ac.at>
2022-08-25 11:40:43 +0200

    Created fies without ocean in data

--- code/compile.py
+++ code/compile.py
@@ -1,16 +1,18 @@
 import numpy as np
-from multiprocessing import Pool
-from netCDF4 import Dataset
-if __name__ == '__main__':
-    df1 = Dataset('../data/grid.nc')
-    mask = df1['subset_flag'][:]
-    lat = df1['lat'][:]
-    lon = df1['lon'][:]
-    mask = np.array(mask)
-    lat = np.array(lat)
-    lon = np.array(lon)
-    lat = lat +.125
-    lon = lon + .125
-    land = np.where(mask==1)
-    indices = np.arange(72)
-    
+#from multiprocessing import Pool
+land = np.load('../data/land_coords.npy')
+indices = np.arange(72)
+indices_arr = np.arange(14400)
+arr = np.load('../data/train_0.npy')
+temp = np.zeros((land.shape[0]*108,arr.shape[1]))
+c = 0
+del(arr)
+for index in indices:
+    arr = np.load('../data/train_%i.npy'%index)
+    for i in indices_arr:
+        lat = arr[i*108][0]
+        lon = arr[i*108][1]
+        if np.any(np.logical_and((lat==land[:,0]),(lon==land[:,1])))==True:
+            temp[c*108:(c+1)*108] = arr[i*108:(i+1)*108]
+            c+=1
+np.save('../data/train_sans_ocean.npy',temp)
