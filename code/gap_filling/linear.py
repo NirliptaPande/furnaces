db41447b3270885fe66cdbe8cf3257347f18024c
Nirlipta Pande <nirlipta.pande@geo.tuwien.ac.at>
2022-10-12 14:56:02 +0200

    linear is running, o/p unclear

--- code/gap_filling/linear.py
+++ code/gap_filling/linear.py
@@ -4,19 +4,46 @@ import numpy as np
 #import matplotlib.pyplot as plt
 from multiprocessing import Pool
 import gc
+from math import nan
 def fill_mean(idx):
       var = spatial[:,0:,0:,idx]
       var_mean = np.nanmean(var,axis = 1)
       np.nan_to_num(var_mean,nan = 0,copy = False)
-      inds = np.where(np.isnan(var))
-      var[inds] = np.take(var_mean,inds[1]
-      )#I don't think this line is correct, or will work for 3D
-      #the above line is supposed to fill in the mean values for each varible, 
-      # but if you can fill in 3D, you should be able to fill in 4D, you shouldn't need a loop
+      for i in range(12):
+            temp = var[:,0:,i]
+            #temp.shape = (244243, 9) (pixels, years)
+            temp_mean = var_mean[:,i]
+            temp_mean = np.reshape(temp_mean,(-1,1))
+            np.nan_to_num(temp_mean,copy= False,nan=0)
+            #temp_mean.shape = (244243,1)
+            inds = np.where(np.isnan(temp))
+            temp[inds] = np.take(temp_mean,inds[1])
+            #I have absolutely no idea what this is, picked it up from stackoverflow
+            #I don't think this line is correct, or will work for 3D
+            #the above line is supposed to fill in the mean values for each varible, 
+            # but if you can fill in 3D, you should be able to fill in 4D, you shouldn't need a loop
+            var[:,0:,i] = temp
       return var
 
 def fill_linear(idx):
-      var = spatial[:,0:,0:,idx]
+      var = np.copy(spatial[:,0:,0:,idx])
+      for i in range(12):
+            arr = var[:,0:,i]
+            arr_int = np.zeros_like(arr,dtype = 'float')
+            test_sum = np.isnan(arr).sum(axis =0)
+            for i in range(arr.shape[1]):
+                  temp = arr[:,i]
+                  test = test_sum[i]
+                  #print(test)
+                  if (test == arr.shape[0]):#If all are false along a column
+                        arr_int[:,i]=0
+                  else:
+                        nans = np.isnan(temp)
+                        x = lambda z:z.nonzero()[0]
+                        temp[nans] = np.interp(x(nans),x(~nans),temp[~nans])
+                        arr_int[:,i]= temp
+            var[:,0:,i] = arr_int
+      return var      
       
 train = np.load('../../data/train_sans_ocean.npy')
 pixels = train.shape[0]//108
@@ -42,14 +69,14 @@ gc.collect()
 """
 I think in the first part, I'll fill in the data with the monthly mean, in all NaNs
 """
-#idxs = range(3,38,1)
-#with Pool() as pool:
-#     mean = pool.map( fill_mean, idxs)
-#pool.close()
-#pool.join()
-#np.save("../../data/month_mean.npy",mean)
-#del(mean)
-#gc.collect()
+idxs = range(3,38,1)
+with Pool() as pool:
+     mean = pool.map( fill_mean, idxs)
+pool.close()
+pool.join()
+np.save("../../data/month_mean.npy",mean)
+del(mean)
+gc.collect()
 """
 0,1,2 don't need any interpolation,
 I'll fill in all the rest with mean of a certain pixel over a certain month, 
@@ -61,4 +88,4 @@ with Pool() as pool:
       inter = pool.map(fill_linear,idxs)
 pool.close()
 pool.join()
-np.save("../../data/linear_month.npy")
+np.save("../../data/linear_month.npy",inter)
