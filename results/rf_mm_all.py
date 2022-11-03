9c5b0aa8f52584e72147b9af6dde24c555e5bae0
Nirlipta <nirlipta.pande@tuwien.ac.at>
2022-11-03 16:52:01 +0100

    vif, pearsons

--- results/rf_mm_all.py
+++ results/rf_mm_all.py
@@ -19,25 +19,25 @@ y_pred = model.predict(x_test)
 err_all_none[0] = np.divide(np.mean(((y_test-y_pred)**2)),np.mean(y_test**2))
 y_pred_mean = np.mean(np.reshape(y_pred,(-1,57)),axis = 1)
 err_all_none[1] = np.divide(np.mean(((y_test_mean-np.mean(y_pred))**2)),y_test_mean**2)
-x = np.linspace(-89.5,90,720)
-y = np.linspace(-179.5,180,1440)
-lons,lats = np.meshgrid(y,x)
-grid = np.zeros((720,1440))
-y_pred_mean = np.reshape(y_pred_mean,(-1,1))
-y_pred_mean = np.append(lon.reshape(-1,1),y_pred_mean,axis = 1)#lon
-y_pred_mean = np.append(lat.reshape(-1,1),y_pred_mean,axis = 1)#lat
-for i in range(y_pred_mean.shape[0]):
-    grid[int(y_pred_mean[:,0][i]+89.5)*4][int(y_pred_mean[:,1][i]+179.5)*4] = y_pred_mean[:,2][i]
-fig = plt.figure(figsize=(35,30), edgecolor='w')
-map = Basemap(projection='cyl',resolution='c',llcrnrlat=-90,urcrnrlat=90,llcrnrlon=-180,urcrnrlon=180)
-map.pcolormesh(lons,lats,grid,cmap = 'plasma')
-map.drawcoastlines(color='lightgray')
-map.drawparallels(np.arange(-90.,91.,15.),color='grey')
-map.drawmeridians(np.arange(-180.,181.,30.),color='grey')
-plt.title(" Predicted Ignitions - all")
-plt.colorbar()
-plt.savefig('../output/month_mean/all')
-plt.close()
+# x = np.linspace(-89.5,90,720)
+# y = np.linspace(-179.5,180,1440)
+# lons,lats = np.meshgrid(y,x)
+# grid = np.zeros((720,1440))
+# y_pred_mean = np.reshape(y_pred_mean,(-1,1))
+# y_pred_mean = np.append(lon.reshape(-1,1),y_pred_mean,axis = 1)#lon
+# y_pred_mean = np.append(lat.reshape(-1,1),y_pred_mean,axis = 1)#lat
+# for i in range(y_pred_mean.shape[0]):
+#     grid[int(y_pred_mean[:,0][i]+89.5)*4][int(y_pred_mean[:,1][i]+179.5)*4] = y_pred_mean[:,2][i]
+# fig = plt.figure(figsize=(35,30), edgecolor='w')
+# map = Basemap(projection='cyl',resolution='c',llcrnrlat=-90,urcrnrlat=90,llcrnrlon=-180,urcrnrlon=180)
+# map.pcolormesh(lons,lats,grid,cmap = 'plasma')
+# map.drawcoastlines(color='lightgray')
+# map.drawparallels(np.arange(-90.,91.,15.),color='grey')
+# map.drawmeridians(np.arange(-180.,181.,30.),color='grey')
+# plt.title(" Predicted Ignitions - all")
+# plt.colorbar()
+# plt.savefig('../output/month_mean/all')
+# plt.close()
 del(model)
 gc.collect()
 
@@ -47,23 +47,23 @@ y_pred = model.predict(x_test)
 err_all_none[2] = np.divide(np.mean(((y_test-y_pred)**2)),np.mean(y_test**2))
 y_pred_mean = np.mean(np.reshape(y_pred,(-1,57)),axis = 1)
 err_all_none[3] = np.divide(np.mean(((y_test_mean-np.mean(y_pred))**2)),y_test_mean**2)
-x = np.linspace(-89.5,90,720)
-y = np.linspace(-179.5,180,1440)
-lons,lats = np.meshgrid(y,x)
-grid = np.zeros((720,1440))
-y_pred_mean = np.reshape(y_pred_mean,(-1,1))
-y_pred_mean = np.append(lon.reshape(-1,1),y_pred_mean,axis = 1)#lon
-y_pred_mean = np.append(lat.reshape(-1,1),y_pred_mean,axis = 1)#lat
-for i in range(y_pred_mean.shape[0]):
-    grid[int(y_pred_mean[:,0][i]+89.5)*4][int(y_pred_mean[:,1][i]+179.5)*4] = y_pred_mean[:,2][i]
-fig = plt.figure(figsize=(35,30), edgecolor='w')
-map = Basemap(projection='cyl',resolution='c',llcrnrlat=-90,urcrnrlat=90,llcrnrlon=-180,urcrnrlon=180)
-map.pcolormesh(lons,lats,grid,cmap = 'plasma')
-map.drawcoastlines(color='lightgray')
-map.drawparallels(np.arange(-90.,91.,15.),color='grey')
-map.drawmeridians(np.arange(-180.,181.,30.),color='grey')
-plt.title(" Predicted Ignitions - none")
-plt.colorbar()
-plt.savefig('../output/month_mean/none.png')
-plt.close()
+# x = np.linspace(-89.5,90,720)
+# y = np.linspace(-179.5,180,1440)
+# lons,lats = np.meshgrid(y,x)
+# grid = np.zeros((720,1440))
+# y_pred_mean = np.reshape(y_pred_mean,(-1,1))
+# y_pred_mean = np.append(lon.reshape(-1,1),y_pred_mean,axis = 1)#lon
+# y_pred_mean = np.append(lat.reshape(-1,1),y_pred_mean,axis = 1)#lat
+# for i in range(y_pred_mean.shape[0]):
+#     grid[int(y_pred_mean[:,0][i]+89.5)*4][int(y_pred_mean[:,1][i]+179.5)*4] = y_pred_mean[:,2][i]
+# fig = plt.figure(figsize=(35,30), edgecolor='w')
+# map = Basemap(projection='cyl',resolution='c',llcrnrlat=-90,urcrnrlat=90,llcrnrlon=-180,urcrnrlon=180)
+# map.pcolormesh(lons,lats,grid,cmap = 'plasma')
+# map.drawcoastlines(color='lightgray')
+# map.drawparallels(np.arange(-90.,91.,15.),color='grey')
+# map.drawmeridians(np.arange(-180.,181.,30.),color='grey')
+# plt.title(" Predicted Ignitions - none")
+# plt.colorbar()
+# plt.savefig('../output/month_mean/none.png')
+# plt.close()
 np.save('../output/month_mean/err_all_none.npy',err_all_none)
